4eba8bd9ab23da2415b9a6a798b5ac090d2f5127
BMM <m.bazyk@gmail.com>
2026-01-11 18:11:57 +0200

    update note

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -24,12 +24,13 @@ function NoteDetailsClient() {
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note?.title}</h2>
+          <h2>{note.title}</h2>
         </div>
         <p className={css.content}>{note?.content}</p>
         <p className={css.date}>
           {new Date(note.createdAt).toLocaleDateString()}
         </p>
+        <p className={css.tag}>{note.tag}</p>
       </div>
     </div>
   );
